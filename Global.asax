4748aadbe2d17881079c9fba2883cf52499efbb3
kalam003 <159292007+kalam003@users.noreply.github.com>
2024-02-14 18:07:49 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ravisupermarket.MvcApplication" Language="C#" %>
---
+++ tbl_employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ravisupermarket
+{
+    public class tbl_employee
+    { public int Id { get; set; }   
+        public int emp_id { get; set; }
+        public string emp_name { get; set;}
+        public string emp_mob { get; set;}
+        public string emp_salary {  get; set;}
+    }
+}
